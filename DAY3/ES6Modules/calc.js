a42556767e7eb5b14082d56cff82dac41f7e21e0
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-03-07 16:30:28 +0530

    ✨ ES6 Modules

---
+++ DAY3/ES6Modules/calc.js
@@ -0,0 +1,3 @@
+import Divide, { Add as Addition, Multiply } from "./math.js";
+console.log("The addition is : ", Addition(20, 30));
+console.log(Multiply(20, 30));
---
+++ DAY3/ES6Modules/math.js
@@ -0,0 +1,9 @@
+export function Add(x, y) {
+  return x + y;
+}
+export function Multiply(x, y) {
+  return x * y;
+}
+export default function Divide(x, y) {
+  return x / y;
+}
